54923cc8995afd4c5a4b11225baa2f4d718f6b41
Jay Jadav <jayjadav9313@gmail.com>
2026-08-03 11:59:16 +0530

    swap inline svg logo with image logo

--- frontend/src/components/site/SiteHeader.jsx
+++ frontend/src/components/site/SiteHeader.jsx
@@ -123,56 +123,15 @@ export default function SiteHeader() {
   return (
     <header className="site-header-new">
       <div className="container header-container-new">
-        {/* Left Tilted Diamond Logo */}
         <Link to="/" className="brand-logo-new" onClick={() => setMenuOpen(false)}>
-          <svg className="logo-3d-svg" width="40" height="40" viewBox="10 10 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <linearGradient id="grey-face-1" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor="#d1d5db" />
-                <stop offset="100%" stopColor="#9ca3af" />
-              </linearGradient>
-              <linearGradient id="grey-face-2" x1="0" y1="0" x2="1" y2="0">
-                <stop offset="0%" stopColor="#9ca3af" />
-                <stop offset="100%" stopColor="#4b5563" />
-              </linearGradient>
-              <linearGradient id="orange-face-1" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor="#ff9f67" />
-                <stop offset="100%" stopColor="#ea580c" />
-              </linearGradient>
-              <linearGradient id="orange-face-2" x1="0" y1="0" x2="1" y2="0">
-                <stop offset="0%" stopColor="#f97316" />
-                <stop offset="100%" stopColor="#b91c1c" />
-              </linearGradient>
-            </defs>
-
-            {/* Left/Back Grey 3D Block */}
-            <g className="block-grey">
-              {/* Top Face */}
-              <path d="M22 38 L38 28 L54 38 L38 48 Z" fill="#e5e7eb" />
-              {/* Left Face */}
-              <path d="M22 38 L38 48 L38 78 L22 68 Z" fill="url(#grey-face-1)" />
-              {/* Right Face */}
-              <path d="M38 48 L54 38 L54 68 L38 78 Z" fill="url(#grey-face-2)" />
-              {/* Bending extension top */}
-              <path d="M38 28 L54 18 L70 28 L54 38 Z" fill="#f3f4f6" />
-              {/* Bending extension right face */}
-              <path d="M54 38 L70 28 L70 58 L54 68 Z" fill="url(#grey-face-2)" />
-            </g>
-
-            {/* Right/Front Orange 3D Block */}
-            <g className="block-orange">
-              {/* Top Face */}
-              <path d="M42 58 L58 48 L74 58 L58 68 Z" fill="#ffedd5" />
-              {/* Left Face */}
-              <path d="M42 58 L58 68 L58 98 L42 88 Z" fill="url(#orange-face-1)" />
-              {/* Right Face */}
-              <path d="M58 68 L74 58 L74 88 L58 98 Z" fill="url(#orange-face-2)" />
-              {/* Bending extension top */}
-              <path d="M58 48 L74 38 L90 48 L74 58 Z" fill="#ffedd5" />
-              {/* Bending extension right face */}
-              <path d="M74 58 L90 48 L90 78 L74 88 Z" fill="url(#orange-face-2)" />
-            </g>
-          </svg>
+          <img 
+            className="logo-3d-svg" 
+            src="/favicon.png" 
+            alt="Transpower Logo" 
+            width="40" 
+            height="40" 
+            style={{ objectFit: 'contain' }}
+          />
           <div className="logo-text-wrapper">
             <span className="logo-text-new">TRANSPOWER</span>
             <span className="logo-subtext-new">Technologies Pvt. Ltd.</span>
